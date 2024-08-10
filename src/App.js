f2988736a9a2ee98542e113804043fcb81fc6964
fidanrehimli <frhimli6@gmail.com>
2024-08-10 21:22:09 +0400

    api create

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Sidebar from "./components/Sidebar/Sidebar";
-<<<<<<< HEAD
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
-import Calendar from "./pages/Calendar";
-import Tasks from "./pages/Tasks";
-import Login from "./pages/Login";
-import AppointmentForm from "./pages/AppointmentForm";
-=======
+// import Dashboard from "./pages/Dashboard";
+// import Customers from "./pages/Customers";
+// import Calendar from "./pages/Calendar";
+// import Tasks from "./pages/Tasks";
+// import Login from "./pages/Login";
+// import AppointmentForm from "./pages/AppointmentForm";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Customers from "./pages/customers/Customers";
 import Calendar from "./pages/calendar/Calendar";
@@ -16,7 +14,6 @@ import Tasks from "./pages/tasks/Tasks";
 import Login from "./pages/login/Login";
 import AppointmentForm from "./pages/appointmentform/AppointmentForm";
 import NewCustomer from "./components/newCustomer/NewCustomer";
->>>>>>> 615f1139e1a92bf6c19194338cdfed55fd5468be
 
 function App() {
   return (
@@ -27,13 +24,10 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/calendar/*" element={<Calendar />} />
           <Route path="/tasks" element={<Tasks />} />
-<<<<<<< HEAD
 
           <Route path="/login" element={<Login/>} />
-=======
           <Route path="/login" element={<Login />} />
           <Route path="/newCustomer" element={<NewCustomer />} />
->>>>>>> 615f1139e1a92bf6c19194338cdfed55fd5468be
           <Route path="/appointment-form" element={<AppointmentForm />} />
         </Routes>
       </Sidebar>
